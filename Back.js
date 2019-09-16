18766bfcae757d53ec0249fc4aa0e7289fd81a3a
Rij Dorfman <rdorfman419@outlook.com>
2019-09-16 11:27:45 -0700

    Time works

---
+++ Back.js
@@ -0,0 +1,24 @@
+
+var timeIn, timeOut;
+
+
+document.querySelector ('.In').addEventListener('click', function() {
+
+	//var actualtime = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+	let d = new Date();
+	console.log(d);
+	year = d.getYear();
+	month = d.getMonth();
+	day = d.getDay();
+	hour = d.getHours();
+	minute = d.getMinutes();
+	document.querySelector('.time-in').textContent = month;
+
+});
+
+document.querySelector ('.Out').addEventListener('click', function() {
+
+	let d = new Date();
+	console.log(d);
+	document.querySelector('.time-out').textContent = d;
+});
